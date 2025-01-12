d109de6a1dbb71ebc4a9e3fc9ad72c0a77e14291
purushothamvinodam <purushothamvinodam@gmail.com>
2025-01-12 09:03:30 +0530

    get Method

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -3,7 +3,12 @@ package com.example.demo;
 public class Demo {
 	public static void main(String []ar) {
 		System.out.println("Hello ....!!!");
-		System.out.println("Hi ....");
+		System.out.print("Hi ");
+		System.out.println(get("purushotham"));
+	}
+	public static String get(String r) {
+		String s=r;
+		return s;
 	}
 
 }
