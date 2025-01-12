dd4fedd1e1dc7b7ce65e5a88b81df15556c783d6
purushothamvinodam <purushothamvinodam@gmail.com>
2025-01-12 08:39:25 +0530

    first commit

---
+++ src/main/java/com/example/demo/HellowordApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowordApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world .... !!!!!");
+		SpringApplication.run(HellowordApplication.class, args);
+	}
+
+}
