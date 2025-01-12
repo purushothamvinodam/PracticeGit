36885556b153b760a7ec9cd197ea21a1558bffaa
purushothamvinodam <purushothamvinodam@gmail.com>
2025-01-12 12:59:39 +0530

    to r

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -6,8 +6,8 @@ public class Demo {
 		System.out.print("Hi ");
 		System.out.println(get("purushotham"));
 	}
-	public static String get(String r2) {
-		String s1=r2;
+	public static String get(String r) {
+		String s1=r;
 		return s1;
 	}
 
