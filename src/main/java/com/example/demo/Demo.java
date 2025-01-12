f0ada8b19bc817f2aded58624a6022ee544550c2
purushothamvinodam <purushothamvinodam@gmail.com>
2025-01-12 12:25:34 +0530

    r1 to r2

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -6,8 +6,8 @@ public class Demo {
 		System.out.print("Hi ");
 		System.out.println(get("purushotham"));
 	}
-	public static String get(String r1) {
-		String s1=r1;
+	public static String get(String r2) {
+		String s1=r2;
 		return s1;
 	}
 
