b33d9ef667827e11e4f5695059fb162d6c6fa291
purushothamvinodam <purushothamvinodam@gmail.com>
2025-01-15 11:34:20 +0530

    removed sop

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class Demo {
 	public static void main(String []ar) {
-		System.out.println("Hello ....!!!");
+
 		System.out.print("Hi ");
 		System.out.println(get("purushotham"));
 	}
