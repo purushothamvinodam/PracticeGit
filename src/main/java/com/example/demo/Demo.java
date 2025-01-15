fed3abc4d7d0ef0ea7645a48c12331a01580268d
purushothamvinodam <purushothamvinodam@gmail.com>
2025-01-15 11:42:21 +0530

    removed sop hi

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -2,8 +2,8 @@ package com.example.demo;
 
 public class Demo {
 	public static void main(String []ar) {
+		System.out.println("hello  ");
 
-		System.out.print("Hi ");
 		System.out.println(get("purushotham"));
 	}
 
